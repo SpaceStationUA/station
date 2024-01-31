ad1de3bd35d167346b2bb490218580f6e6497e1f
Dan <jamasty@protonmail.com>
2024-01-31 11:37:22 +0200

    feat: new frontiers fleet and stations

--- Content.Shared/Shuttles/Components/IFFComponent.cs
+++ Content.Shared/Shuttles/Components/IFFComponent.cs
@@ -48,5 +48,9 @@ public enum IFFFlags : byte
     /// </summary>
     Hide,
 
+    /// <summary>
+    /// Is this a player shuttle
+    /// </summary>
+    IsPlayerShuttle = 4,
     // TODO: Need one that hides its outline, just replace it with a bunch of triangles or lines or something.
 }
