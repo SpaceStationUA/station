b758a22bef9912f59bf3b685f3d6b2d916fe2efb
v0id <v0idRift@github.com>
2024-06-01 16:53:01 +0300

    fix DetailExaminableComponent

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -191,7 +191,7 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
             _metaSystem.SetEntityName(entity.Value, profile.Name);
             if (profile.FlavorText != "" && _configurationManager.GetCVar(CCVars.FlavorText))
             {
-                AddComp<DetailExaminableComponent>(entity.Value).Content = profile.FlavorText;
+                EnsureComp<DetailExaminableComponent>(entity.Value).Content = profile.FlavorText;
             }
         }
 
