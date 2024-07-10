0163d0077142d5e1fdf96622c7f9e37324476fa7
v0id <v0idRift@github.com>
2024-07-10 13:51:18 +0300

    Added Mercenary

---
+++ Content.Server/_Pirate/Mercenary/Commands/MakeAMerc.cs
@@ -0,0 +1,56 @@
+using Content.Server._Pirate.MakeATraitor;
+using Content.Server.Administration;
+using Content.Server.Administration.Logs;
+using Content.Shared.Administration;
+using Robust.Shared.Console;
+
+namespace Content.Server._Pirate.Mercenary.Commands;
+
+[AdminCommand(AdminFlags.Admin)]
+public sealed class MakeAMerc: IConsoleCommand
+{
+    [Dependency] private readonly IAdminLogManager _adminLogger = default!;
+    [Dependency] private readonly EntityManager EntityManager = default!;
+
+    // [Dependency] private readonly ThiefRuleSystem _thief = default!;
+    // [Dependency] private readonly TraitorRuleSystem _traitorRule = default!;
+    // [Dependency] private readonly RevolutionaryRuleSystem _revolutionaryRule = default!;
+
+    public string Command => "makemerc";
+
+    public string Description => "робить вибраного гравця найманцем";
+
+    public string Help => "makemerc <uid>";
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var player = shell.Player;
+        if (player != null)
+        {
+            shell.WriteLine("You cannot use this command from the player.");
+            return;
+        }
+
+        if (args.Length != 1)
+        {
+            shell.WriteLine(Loc.GetString("shell-wrong-arguments-number"));
+            return;
+        }
+
+        if (!int.TryParse(args[0], out var intUid))
+        {
+            shell.WriteError("uid must be a number");
+            return;
+        }
+
+        var targetNet = new NetEntity(intUid);
+
+        if (!EntityManager.TryGetEntity(targetNet, out var target))
+        {
+            shell.WriteError("cannot find entity");
+            return;
+        }
+
+        EntityManager.System<MakeAMercSystem>().MakeAnMerc(target.Value);
+    }
+}
---
+++ Content.Server/_Pirate/Mercenary/MakeAMercSystem.cs
@@ -0,0 +1,62 @@
+using Content.Server.Ghost.Roles.Components;
+using Content.Server.Humanoid.Systems;
+using Content.Server.RandomMetadata;
+using Content.Server.RoundEnd;
+using Content.Shared.Mind;
+using Content.Shared.Players;
+using Content.Shared.Shuttles.Components;
+using Robust.Server.GameObjects;
+using Robust.Server.Maps;
+using Robust.Server.Player;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Server._Pirate.Mercenary;
+
+public sealed class MakeAMercSystem : EntitySystem
+{
+    private const string MapPath = "Maps/Shuttles/Qazmlp/st_merc.yml";
+    [ValidatePrototypeId<EntityPrototype>] private const string SpawnerPrototypeId = "Mercenary";
+    [Dependency] private readonly IEntityManager _entManager = default!;
+    [Dependency] private readonly MapLoaderSystem _map = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly RandomHumanoidSystem _randomHumanoidSystem = default!;
+    [Dependency] private readonly RandomMetadataSystem _randomMetadataSystem = default!;
+    [Dependency] private readonly RoundEndSystem _roundEndSystem = default!;
+
+    public void MakeAnMerc(EntityUid entity)
+    {
+        _playerManager.TryGetSessionByEntity(entity, out var session);
+
+        if (session is null)
+            return;
+
+        var playerCData = session.ContentData();
+        if (playerCData == null)
+            return;
+
+        var mindSystem = _entManager.System<SharedMindSystem>();
+        var metadata = _entManager.GetComponent<MetaDataComponent>(entity);
+        var mind = playerCData.Mind ?? mindSystem.CreateMind(session.UserId, metadata.EntityName);
+
+        var shuttleMap = _mapManager.CreateMap();
+        var options = new MapLoadOptions
+        {
+            LoadMap = true
+        };
+
+        if (!_map.TryLoad(shuttleMap, MapPath, out var grids, options))
+            return;
+
+        var shuttle = grids.FirstOrNull();
+        if (shuttle == null)
+            return;
+
+        var spawn = Transform(shuttle.Value).Coordinates;
+        var uid = _randomHumanoidSystem.SpawnRandomHumanoid(SpawnerPrototypeId, spawn, metadata.EntityName);
+        RemComp<GhostRoleComponent>(uid);
+        mindSystem.TransferTo(mind, uid, true);
+    }
+}
