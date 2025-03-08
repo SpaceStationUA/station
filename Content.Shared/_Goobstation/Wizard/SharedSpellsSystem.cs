1ff9f69caba5bba5702c90d022c939313367e451
v0id <>
2025-03-08 13:49:11 +0200

    Wizard patch 2 (#1846)
    * Buff armor.
    
    * Upgrade tweaks.
    
    * Fix cryo katana contra status.
    
    * Faster jaunt.
    
    * Buff mugwort.
    
    * Buff gorilla form.
    
    * Traps and forcewall tweak.
    
    * Fix.
    
    * Fix invisible ghosts on summon.
    
    * Wiz mirror fixes.
    
    * Fixes.
    
    * I forgot.
    
    * Fun.
    
    ---------
    
    Co-authored-by: Aidenkrz <aiden@djkraz.com>
    # Conflicts:
    #	Content.Server/Chat/Systems/ChatSystem.cs
    #	Content.Shared/_Goobstation/Wizard/SharedSpellsSystem.cs
    #	Resources/Prototypes/Entities/Mobs/Player/silicon.yml
    #	Resources/Prototypes/Entities/Objects/Weapons/Guns/Projectiles/magic.yml
    #	Resources/Prototypes/_Goobstation/Entities/Mobs/Player/silicon.yml
    #	Resources/Prototypes/_Goobstation/Entities/Objects/Weapons/Melee/cryokatana.yml
    #	Resources/Prototypes/borg_types.yml

--- Content.Shared/_Goobstation/Wizard/SharedSpellsSystem.cs
+++ Content.Shared/_Goobstation/Wizard/SharedSpellsSystem.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Numerics;
+using Content.Shared._Goobstation.Bingle;
 using Content.Shared._Goobstation.Wizard.BindSoul;
 using Content.Shared._Goobstation.Wizard.Chuuni;
 using Content.Shared._Goobstation.Wizard.Components;
@@ -1250,9 +1251,10 @@ public abstract class SharedSpellsSystem : EntitySystem
         var bodyPartQuery = GetEntityQuery<BodyPartComponent>();
         var inventoryQuery = GetEntityQuery<InventoryComponent>();
         var handsQuery = GetEntityQuery<HandsComponent>();
+        var binglePitQuery = GetEntityQuery<BinglePitComponent>();
 
         while (parent.IsValid() && !bodyQuery.HasComp(parent) && !bodyPartQuery.HasComp(parent) &&
-               !inventoryQuery.HasComp(parent) && !handsQuery.HasComp(parent))
+               !inventoryQuery.HasComp(parent) && !handsQuery.HasComp(parent) && !binglePitQuery.HasComp(parent))
         {
             if (((EntityManager.MetaQuery.GetComponent(child).Flags & MetaDataFlags.InContainer) ==
                  MetaDataFlags.InContainer) && managerQuery.TryGetComponent(parent, out var conManager) &&
