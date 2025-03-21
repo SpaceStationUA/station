3536f3465dfbc30b997c0efee132b81c9a655e99
CarryUa <mosuk81@gmail.com>
2025-03-21 08:28:03 +0100

    minor changes

--- Content.Server/_Pirate/Power/Components/IPCChargerComponent.cs
+++ Content.Server/_Pirate/Power/Components/IPCChargerComponent.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using Robust.Shared.Audio;
 
 namespace Content.Server._Pirate.Power.Components
-{.
+{
     [RegisterComponent]
     public sealed partial class IPCChargerComponent : Component
     {
--- Content.Server/_Pirate/Power/EntitySystems/IPCChargerSystem.cs
+++ Content.Server/_Pirate/Power/EntitySystems/IPCChargerSystem.cs
@@ -5,6 +5,7 @@ using Content.Server.Power.EntitySystems;
 using Content.Shared.Containers.ItemSlots;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Power;
+using Robust.Shared.Audio;
 using Robust.Shared.Audio.Systems;
 
 namespace Content.Server._Pirate.Power.EntitySystems
@@ -27,9 +28,10 @@ namespace Content.Server._Pirate.Power.EntitySystems
             if (!TryComp(itemSlot_component.Slots["cell_slot"].Item, out BatteryComponent? batteryComponent)) // checks if the player actually have cell inserted
                 return;
             var appearance = EntityManager.GetComponentOrNull<AppearanceComponent>(uid);
-            _appearence.SetData(uid, PowerChargeVisuals.Active, 0);
+            var soundSpec = new SoundPathSpecifier("/Audio/Effects/PowerSink/charge_fire.ogg");
+            _appearence.SetData(uid, PowerChargeVisuals.Active, PowerChargeStatus.Off); //TODO: Fix visuals changing
             _battery.SetCharge(args.User, batteryComponent.CurrentCharge + component.ChargeAmount, batteryComponent);
-            _sound.PlayPvs("/Audio/Effects/PowerSink/charge_fire.ogg", uid);
+            _sound.PlayPvs(soundSpec, uid);
             component.Useable = false;
         }
     }
