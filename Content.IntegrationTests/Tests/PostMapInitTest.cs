25ac9c28350fb90bc3972bde31a48d685ac70a15
LuciusGG <LuciusGodGameTP@gmail.com>
2025-04-17 21:40:45 +0300

    Gate map

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -74,7 +74,8 @@ namespace Content.IntegrationTests.Tests
             "Lambda", // Maintained by Ichai
             "Bagel", // Maintained by Ichai
             "Northway", // Maintained by Violet
-            "Anchor" // Maintained by the SiN Mapping Team.
+            "Anchor", // Maintained by the SiN Mapping Team.
+            "Gate"
         };
 
         /// <summary>
